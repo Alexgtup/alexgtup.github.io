1877b276bc32354c241e135c3c59f223f64df616
Alexgtup <117043593+Alexgtup@users.noreply.github.com>
2026-09-10 14:46:20 +0800

    Bust UX asset cache after interaction pass

--- scripts/stage24_layout_system.py
+++ scripts/stage24_layout_system.py
@@ -13,8 +13,8 @@ links = (
     ('data-stage89-guide-rhythm="true"', '<link href="/assets/stage89-guide-rhythm.css?v=20260909-2" rel="stylesheet" data-stage89-guide-rhythm="true"/>'),
     ('data-stage83-mobile-actions="true"', '<link href="/assets/stage83-mobile-actions.css?v=20260909-4" rel="stylesheet" data-stage83-mobile-actions="true"/>'),
     ('data-stage84-mobile-runtime="true"', '<script defer src="/assets/stage84-mobile-runtime.js?v=20260909-2" data-stage84-mobile-runtime="true"></script>'),
-    ('data-stage94-site-ux-css="true"', '<link href="/assets/stage94-site-ux.css?v=20260909-1" rel="stylesheet" data-stage94-site-ux-css="true"/>'),
-    ('data-stage94-site-ux-js="true"', '<script defer src="/assets/stage94-site-ux.js?v=20260909-4" data-stage94-site-ux-js="true"></script>'),
+    ('data-stage94-site-ux-css="true"', '<link href="/assets/stage94-site-ux.css?v=20260910-2" rel="stylesheet" data-stage94-site-ux-css="true"/>'),
+    ('data-stage94-site-ux-js="true"', '<script defer src="/assets/stage94-site-ux.js?v=20260910-5" data-stage94-site-ux-js="true"></script>'),
 )
 
 all_html = sorted(root.rglob("*.html"))
